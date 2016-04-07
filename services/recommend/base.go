4ed72216b118f06c700d349cd24c0765c6d4176b
Therin Irwin <therin.irwin@gmail.com>
2016-04-07 14:15:28 -0700

    Move Find Perks code into separate file

--- services/recommend/base.go
+++ services/recommend/base.go
@@ -2,8 +2,6 @@ package recommend
 
 import (
 	"fmt"
-	"sort"
-
 	"github.com/muesli/regommend"
 	"github.com/yiziz/collab/services/data"
 )
@@ -35,7 +33,7 @@ func Bar() {
 	fmt.Println(Terms.Count())
 	// fmt.Println(terms)
 
-	for uid := range um {
+	for uid := range Um {
 		recs, _ := Terms.Recommend(uid)
 		// if len(recs) < 100 {
 		if len(recs) != 0 {
@@ -43,54 +41,15 @@ func Bar() {
 			//fmt.Println(recs[:10])
 			// fmt.Println(recs)
 		}
-		neighbors, _ := Terms.Neighbors(uid)
-		if len(neighbors) != 0 {
-			// if len(neighbors) < 100 {
-			// 	fmt.Println(uid)
-			//fmt.Println("neighbors: ", uid, " ", len(neighbors))
-			fmt.Println(neighbors[:10])
-			// 	fmt.Println(neighbors)
 
-			unseenPerks := make(map[uint64]float64)
-
-			for _, neighbor := range neighbors[:5] {
-				nuid := neighbor.Key.(uint64)
-				score := neighbor.Distance
-
-				if score > 0.85 {
-					for perkId, perkNeighborScore := range Rm[nuid] {
-						youScore, ok := Rm[uid][perkId]
-						unseenPerks[perkId] += float64(int64(perkNeighborScore))
-
-						if ok {
-							unseenPerks[perkId] -= float64(int64(youScore))
-						}
-					}
-				}
-			}
-
-			for perkId, _ := range Rm[uid] {
-				delete(unseenPerks, perkId)
-			}
-
-			var scoreArray data.WordScoreArray
-			for perkId, score := range unseenPerks {
-				scoreObj := new(data.WordScore)
-				scoreObj.PerkID = perkId
-				scoreObj.Score = score
-
-				scoreArray = append(scoreArray, scoreObj)
-			}
-
-			sort.Stable(sort.Reverse(scoreArray))
-
-			for _, score := range scoreArray {
-				fmt.Print("{id:", score.PerkID, ", score:", score.Score, "}")
-			}
-			fmt.Println("Id:", uid, "data:", Rm[uid])
-		}
+		perks := PerkByUser(uid)
+		fmt.Println(perks)
 	}
 	fmt.Println("end")
+}
+
+func FindPerksForUser(uid uint64) {
+
 }
 
 // Foo blah
--- services/recommend/recommend.go
+++ services/recommend/recommend.go
@@ -1,13 +1,63 @@
 package recommend
 
+import (
+	"fmt"
+	"github.com/yiziz/collab/services/data"
+	"sort"
+)
+
 // PerkByUser returns perk id recommendations using uid
 func PerkByUser(uid uint64) []uint64 {
-	return []uint64{}
+	var perks []uint64
+	neighbors, _ := Terms.Neighbors(uid)
+
+	if len(neighbors) != 0 {
+		unseenPerks := make(map[uint64]float64)
+
+		for _, neighbor := range neighbors[:5] {
+			nuid := neighbor.Key.(uint64)
+			score := neighbor.Distance
+
+			if score > 0.85 {
+				for perkId, perkNeighborScore := range Rm[nuid] {
+					youScore, ok := Rm[uid][perkId]
+					unseenPerks[perkId] += float64(int64(perkNeighborScore))
+
+					if ok {
+						unseenPerks[perkId] -= float64(int64(youScore))
+					}
+				}
+			}
+		}
+
+		for perkId, _ := range Rm[uid] {
+			delete(unseenPerks, perkId)
+		}
+
+		var scoreArray data.WordScoreArray
+		for perkId, score := range unseenPerks {
+			scoreObj := new(data.WordScore)
+			scoreObj.PerkID = perkId
+			scoreObj.Score = score
+
+			scoreArray = append(scoreArray, scoreObj)
+		}
+
+		sort.Stable(sort.Reverse(scoreArray))
+
+		for _, score := range scoreArray {
+			fmt.Print("{id:", score.PerkID, ", score:", score.Score, "}")
+			perks = append(perks, score.PerkID)
+		}
+		fmt.Println("Id:", uid, "data:", Rm[uid])
+	}
+
+	return perks
 }
 
 // PerkByPerk returns perk id recommendations using pid
 func PerkByPerk(pid uint64) []uint64 {
-	return []uint64{}
+
 }
 
 // PerkByTerms returns perk id recommendations using terms (sl)
