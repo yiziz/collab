a9cda51af8d23d6313f3ae89b8e1b075d8038aca
Therin Irwin <therin.irwin@gmail.com>
2016-04-07 14:04:05 -0700

    Get perk recommendations for a user

--- services/recommend/base.go
+++ services/recommend/base.go
@@ -2,6 +2,7 @@ package recommend
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/muesli/regommend"
 	"github.com/yiziz/collab/services/data"
@@ -10,42 +11,83 @@ import (
 // RecMap by regommend Add as the second param
 type RecMap map[interface{}]float64
 
+var Rm data.UserPerkRedemp
+var Pm data.PerkTermScore
+var Um data.UserTermScore
+var Terms *regommend.RegommendTable
+
 // Bar blah
 func Bar() {
-	rm := data.RedempData()
-	pm := data.PerkTermScores()
-	um := data.CalcUserTermScore(rm, pm)
+	Rm = data.RedempData()
+	Pm = data.PerkTermScores()
+	Um = data.CalcUserTermScore(Rm, Pm)
 
-	terms := regommend.Table("terms")
-	for uid, termMap := range um {
+	Terms = regommend.Table("terms")
+
+	for uid, termMap := range Um {
 		rm := make(RecMap)
 		for term, score := range termMap {
 			rm[term] = float64(score)
 		}
-		terms.Add(uid, rm)
-		// fmt.Println(uid, ": ", rm)
-		if uid == 301171 || uid == 480566 {
-			fmt.Println(uid, ": ", rm)
-		}
+		Terms.Add(uid, rm)
 	}
 
-	fmt.Println(terms.Count())
+	fmt.Println(Terms.Count())
 	// fmt.Println(terms)
 
 	for uid := range um {
-		recs, _ := terms.Recommend(uid)
+		recs, _ := Terms.Recommend(uid)
 		// if len(recs) < 100 {
 		if len(recs) != 0 {
-			fmt.Println("recs: ", uid, " ", len(recs))
+			//fmt.Println("recs: ", uid, " ", len(recs))
+			//fmt.Println(recs[:10])
 			// fmt.Println(recs)
 		}
-		neighbors, _ := terms.Neighbors(uid)
+		neighbors, _ := Terms.Neighbors(uid)
 		if len(neighbors) != 0 {
 			// if len(neighbors) < 100 {
 			// 	fmt.Println(uid)
-			fmt.Println("neighbors: ", uid, " ", len(neighbors))
+			//fmt.Println("neighbors: ", uid, " ", len(neighbors))
 			fmt.Println(neighbors[:10])
 			// 	fmt.Println(neighbors)
+
+			unseenPerks := make(map[uint64]float64)
+
+			for _, neighbor := range neighbors[:5] {
+				nuid := neighbor.Key.(uint64)
+				score := neighbor.Distance
+
+				if score > 0.85 {
+					for perkId, perkNeighborScore := range Rm[nuid] {
+						youScore, ok := Rm[uid][perkId]
+						unseenPerks[perkId] += float64(int64(perkNeighborScore))
+
+						if ok {
+							unseenPerks[perkId] -= float64(int64(youScore))
+						}
+					}
+				}
+			}
+
+			for perkId, _ := range Rm[uid] {
+				delete(unseenPerks, perkId)
+			}
+
+			var scoreArray data.WordScoreArray
+			for perkId, score := range unseenPerks {
+				scoreObj := new(data.WordScore)
+				scoreObj.PerkID = perkId
+				scoreObj.Score = score
+
+				scoreArray = append(scoreArray, scoreObj)
+			}
+
+			sort.Stable(sort.Reverse(scoreArray))
+
+			for _, score := range scoreArray {
+				fmt.Print("{id:", score.PerkID, ", score:", score.Score, "}")
+			}
+			fmt.Println("Id:", uid, "data:", Rm[uid])
 		}
 	}
 	fmt.Println("end")
