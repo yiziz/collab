47d0b3e3a9829707c421bc793277d6551205182a
Therin Irwin <therin.irwin@gmail.com>
2016-04-07 14:27:12 -0700

    Fix syntax error

--- services/recommend/recommend.go
+++ services/recommend/recommend.go
@@ -58,6 +58,7 @@ func PerkByUser(uid uint64) []uint64 {
 // PerkByPerk returns perk id recommendations using pid
 func PerkByPerk(pid uint64) []uint64 {
 
+	return []uint64{}
 }
 
 // PerkByTerms returns perk id recommendations using terms (sl)
